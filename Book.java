e9a66db5be066b4b79a858c584559d033057c497
davane2510 <108930149+davane2510@users.noreply.github.com>
2022-07-29 11:41:55 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,62 @@
+class Book {
+	
+	private String bookName;
+	private String isbnNumber;
+	private String authorName;
+	private String publisher;
+	
+	public Book(String bookName, String isbnNumber, String authorName, String publisher) {
+		this.bookName = bookName;
+		this.isbnNumber = isbnNumber;
+		this.authorName = authorName;
+		this.publisher = publisher;
+	}
+	
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getIsbnNumber() {
+		return isbnNumber;
+	}
+
+	
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public String getBookInfo() {
+		String description = "Book Name: " + getBookName() + "\n" +
+								"isbnNumber: " + getIsbnNumber() + "\n" +
+								"Author Name: " + getAuthorName() + "\n" +
+								"Publisher: " + getPublisher();
+		return description;
+	}
+	
+	public static void main(String[] args) {
+		
+		Book b = new Book("Attitude Is EveryThing", "AT0089", "Jeff Keller", "HarperCollins");
+		System.out.println("___________________________________");
+		System.out.println("Solution: ");
+		System.out.println(b.getBookInfo());
+	}
+	
+
+}
